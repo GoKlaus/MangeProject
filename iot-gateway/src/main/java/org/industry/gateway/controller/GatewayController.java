9000c7d0b665cf872fb8fa864934a2aa563bfcd4
klaus <klaus1515z@gmail.com>
2022-07-27 17:24:34 +0800

    依赖管理

---
+++ iot-gateway/src/main/java/org/industry/gateway/controller/GatewayController.java
@@ -0,0 +1,16 @@
+package org.industry.gateway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * controller
+ */
+@Controller
+public class GatewayController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
